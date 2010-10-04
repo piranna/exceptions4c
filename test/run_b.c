4a6631c265e180cd6f1cc252d3214837ff0b739d
guillermocalvo <guillermocalvo@71d186cc-da81-11de-b50f-7b9694e7f7ce>
2010-10-04 14:11:30 +0000

    Created tag exceptions4c-2.2.

--- test/run_b.c
+++
@@ -1,7 +0,0 @@
-
-# include "testing.h"
-
-# define NEW SUITE_COLLECTION(consistencySuite)
-# define COLLECTION(RUN_SUITE) RUN_SUITE(a)
-
-END_SUITE_COLLECTION
--- test/run_h.c
+++
@@ -1,7 +0,0 @@
-
-# include "testing.h"
-
-# define NEW SUITE_COLLECTION(integrationSuite)
-# define COLLECTION(RUN_SUITE) RUN_SUITE(h)
-
-END_SUITE_COLLECTION
